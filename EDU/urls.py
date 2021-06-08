26561318114ba80f42c2b4413be7066b7e6cadcc
Siavash Mehran <sia.dev.acc@gmail.com>
2021-06-09 00:54:26 +0430

    "static files" config added

--- EDU/urls.py
+++ EDU/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls.static import static
+from EDU import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('', include('EDU_user.urls')),
     
 ]
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
