b45aa59b1d1d8ab3f8cf552d6c062b06e1397340
Siavash Mehran <sia.dev.acc@gmail.com>
2021-06-27 17:23:22 +0430

    still has issue with user forign key

--- EDU_verify/views.py
+++ EDU_verify/views.py
@@ -1,3 +1,4 @@
+from TOOLS.QuerryDict_generator import querryDict_generator
 from EDU_user.models import User_Auth_State, User_Upload_Files
 from EDU_user.forms import User_Auth_State_Model_Form, User_Upload_Files_Model_Form
 from django.shortcuts import render
@@ -13,7 +14,21 @@ def verify_page(request):
     client_info_form   = User_Auth_State_Model_Form(instance=client_info)
 
     if request.method == 'POST':
-        pass
+        
+        if 'wrongInfo' in request.POST:
+            print(request.POST)
+
+        if 'stu_paye' in request.POST:
+            QD = querryDict_generator(request_POST=request.POST, user=myUser)
+            verified_form = User_Auth_State_Model_Form(QD, initial={'user':myUser})
+
+            if verified_form.is_valid(): 
+                verified_model_instance:User_Auth_State = verified_form.save(commit=False)
+                print()
+                print(verified_model_instance)
+                print()
+            else: print('no '*200)
+            
 
     context = {
         'scans' : client_upload,
